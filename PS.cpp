2f72220f29717c998530828a7cb069ba050f2918
QuocThinh <thinhle2025ak@gmail.com>
2026-04-19 15:49:26 +0700

    01

---
+++ PS.cpp
@@ -0,0 +1,32 @@
+#include "PS.h"
+#include<iostream>
+using namespace std;
+
+void PS::NhapPS(){
+	cin >> this->tu >> this->mau;
+}
+void PS::XuatPS(){
+	cout << tu << "/" << mau;
+}
+int PS::UCLN(){
+	int a, b;
+	if (b == 0){
+		return a;
+	}
+	return UCLN(b, a % b);
+}
+void PS::RutGon(PS &p){
+	int ucln = UCLN(abs(p.tu), abs(p.mau));
+	p.tu /= ucln;
+	p.mau /= ucln;
+	if(p.mau < 0){
+		p.tu = -p.tu;
+		p.mau = -p.mau;
+	}
+}
+PS PS::TongPS(PS& p){
+	PS kq;
+	kq.tu = (tu * p.mau) + (mau * p.tu);
+	kq.mau = mau * p.mau;
+
+}
---
+++ PS.h
@@ -0,0 +1,13 @@
+#pragma once
+class PS
+{
+private:
+	float tu, mau;
+public:
+	void NhapPS();
+	void XuatPS();
+	int UCLN();
+	void RutGon(PS& p);
+	PS TongPS(PS& p);
+};
+
